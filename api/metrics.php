3a9830dc23b84dd2028d8476877208e6cb0ad43c
Juan Carlos Reynoso Zuñiga <116603804+juandev-mx@users.noreply.github.com>
2026-06-19 00:57:26 -0600

    Remove comments for Docker

--- api/metrics.php
+++ api/metrics.php
@@ -4,10 +4,8 @@ header("Content-Type: text/plain; charset=utf-8");
 ob_start();
 require_once __DIR__ . '/../config/database.php';
 ob_end_clean();
-// Detectar de forma inteligente si la petición viene dentro de Docker o desde fuera (Windows)
 $es_contenedor = file_exists('/.dockerenv') || (gethostname() === 'qr-attendance-app') || (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Prometheus') !== false);
 
-// Si es Docker usa el alias 'mysql' y puerto 3306. Si es Windows usa '127.0.0.1' y puerto 3307
 $host_env = $es_contenedor ? 'mysql' : '127.0.0.1'; 
 $port_env = $es_contenedor ? '3306'  : '3307';  
 $db_name  = 'control_asistencias_qr';
